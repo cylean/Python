e92b0cac85ca5acab9d084851f827e54eef6baf7
cylean <jorinyar@gmail.com>
2024-10-31 13:19:17 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+HomeworkFinished = 12
+HomeworkFinishedTime = 1.5
+CourseName = 'Python'
+TimeHomeworkFinished = HomeworkFinishedTime / HomeworkFinished
+print('Курс:', CourseName, 'всего задач:', HomeworkFinished,',','затрачено часов:',HomeworkFinishedTime, 'среднее время выполнения', TimeHomeworkFinished, 'часа.')
