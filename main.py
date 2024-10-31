b1809866dead9d7b265c1da659892a98106d68a6
Sergey Shamarin <jorinyar@gmail.com>
2024-10-31 13:30:32 +0300

    30.10.24

---
+++ main.py
@@ -0,0 +1,18 @@
+
+#1st program
+print(9 ** 0.5 * 5)
+
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+#4rd program
+stroka = '123.456'
+number = float(stroka)
+number = number * 10
+number = int(number % 10)
+print(number)
